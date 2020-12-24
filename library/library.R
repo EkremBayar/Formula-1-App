ca1306643299d91ad64594b3b97fc6df3936a42b
EkremBayar <35973642+EkremBayar@users.noreply.github.com>
2020-12-24 15:11:46 +0300

    library eklendi

---
+++ library/library.R
@@ -0,0 +1,9 @@
+# Libraries ---------------------------------------------------------------
+library(shiny)
+library(shinydashboard)
+library(shinydashboardPlus)
+library(shinyWidgets)
+library(tidyverse)
+library(stringi)
+suppressMessages(suppressWarnings(library(extrafont)))
+suppressMessages(suppressWarnings(ttf_import("./www")))
